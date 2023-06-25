059cb02ba90217d2bb48d1da2bc89afca709644a
bakinton <123785614+bakinton@users.noreply.github.com>
2023-06-25 11:58:50 -0400

    update to db URL

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book_store"
+  //"URI": "mongodb://localhost/book_store"
+
+  "URI": "mongodb+srv://Blessingakintonde:xqeTL84YbE1gSrr7@mongodbserver.br7beto.mongodb.net/book_store?retryWrites=true&w=majority"
 };
